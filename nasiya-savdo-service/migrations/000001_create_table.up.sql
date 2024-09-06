0c251c4e5dbb9d3abf9b7d6b69dd1a6e464d7b68
Najmiddin <najmiddin.salikhov@gmail.com>
2024-09-06 15:39:25 +0500

    2

--- nasiya-savdo-service/migrations/000001_create_table.up.sql
+++ nasiya-savdo-service/migrations/000001_create_table.up.sql
@@ -3,6 +3,19 @@ CREATE TYPE contract_status AS ENUM ('pending', 'finished', 'canceled');
 CREATE TYPE exchange_status AS ENUM ('buy', 'sell');
 
 -- Create tables
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    address TEXT,
+    phone_number VARCHAR(20),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at BIGINT DEFAULT 0
+);
+
 CREATE TABLE storage (
     id UUID PRIMARY KEY,
     user_id UUID,
