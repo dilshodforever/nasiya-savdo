1fdcbc0fa2b56ad379c4accb537c3c09289a09d1
Dilshod <dilshodforeverr@gmail.com>
2024-10-24 10:35:37 +0500

    add time to access token

--- auth-service/api/token/token.go
+++ auth-service/api/token/token.go
@@ -41,7 +41,7 @@ func GenereteAccsessJWTToken(user *pb.UserLoginRes) *Tokens {
 	claims["role"] = "admin"
 	claims["storage_id"] = user.StorageId
 	claims["iat"] = time.Now().Unix()
-	claims["exp"] = time.Now().Add(60 * time.Minute).Unix()
+	claims["exp"] = time.Now().Add(24 * time.Hour).Unix()
 	access, err := accessToken.SignedString([]byte(tokenKey))
 	if err != nil {
 		log.Fatal("error while genereting access token : ", err)
